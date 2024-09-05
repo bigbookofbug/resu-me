92505c44e63e32ac043eccb1c47b7746e20c2448
bigbug <bigbookofbug@proton.me>
2024-09-04 19:25:24 -0500

    almost done w/ bugstyle

--- src/resu_me/bugstyle.clj
+++ src/resu_me/bugstyle.clj
@@ -3,21 +3,65 @@
             [clojure.java.io :as io]
             [resu-me.common :as common]))
 
-;;;too many nested writes i believe - likely need to re-arrange
-;;;stylistically - functions beginning with "write-" should be write-commanded"
 (defn write-header
   [file resume-parsed]
   (common/write-boilerplate file)
   (with-open [wrtr (io/writer file :append true)]
     (.write wrtr
             (str
-             "\\begin{document}\n"
              "\\pagenumbering{gobble}\n"
+             "\\begin{document}\n"
              (common/flush-dir "right"
                                (str "\\setstretch{0.5}\n"
                                     "{\\fontsize{12pt}{12pt}\\selectfont\n"
                                     (common/parse-list
-                                     (get-in resume-parsed [:Personal :contact])
-                                     0
-                                     nil)))
-             "\\vspace{-40pt}\n"))))
+                                     (get-in resume-parsed [:Personal :contact]))"}"))
+             "\\vspace{-40pt}\n"
+             (common/flush-dir "left"
+                               (str "\\setstretch{0.5}\n"
+                                    "{\\fontsize{16pt}{16pt}\\selectfont\n"
+                                    "{\\textbf{"
+                                    (get-in resume-parsed [:Personal :name])"}}}\n"
+                                    common/line-sep))))))
+
+
+(defn write-summary
+  [file resume-parsed]
+  (with-open [wrtr (io/writer file :append true)]
+    (.write wrtr
+            (str
+             (common/flush-dir "left"
+                               (str "\\setstretch{0.5}\n"
+                                    "{\\fontsize{12pt}{12pt}\\selectfont\n"
+                                    (common/parse-summary resume-parsed)
+                                    "}\n"
+                                    "\\newline\n"))))))
+
+(defn write-education
+  [file resume-parsed]
+  (with-open [wrtr (io/writer file :append true)]
+    (.write wrtr
+            (str
+             (common/flush-dir "left"
+                               (str "\\setstretch{0.5}\n"
+                                    "{\\fontsize{12pt}{12pt}\\selectfont\n"
+                                    "\\textbf{Education}}\n"
+                                    common/line-sep))
+             (common/flush-dir "left"
+                               (str "{\\textbf{"
+                                    (common/parse-education resume-parsed :institute)
+                                    " \\hfill "
+                                    (common/parse-education resume-parsed :end)
+                                    "}}\n"
+                                    "\\newline\n"
+                                    (common/parse-education resume-parsed :degree) ", "
+                                    (common/parse-education resume-parsed :area)
+                                    (if (not (nil?
+                                              (common/parse-education resume-parsed :highlights)))
+                                      (do (str
+                                           "\\begin{multicols}{2}\n"
+                                           "\\begin{itemize}\n"
+                                           (common/parse-list
+                                            (get-in resume-parsed [:Education_Section :highlights]))
+                                           "\\end{itemize}\n"
+                                           "\\end{multicols}\n")))))))))
--- src/resu_me/common.clj
+++ src/resu_me/common.clj
@@ -16,6 +16,7 @@
   right=2.5cm,
   bottom=2.5cm, bindingoffset=0.5cm]{geometry}
 \\usepackage{soul}
+\\usepackage{multicol}
 \\renewcommand{\\baselinestretch}{1}
 "))))
 
@@ -26,11 +27,30 @@
          body
          "\\end{flush" direction "}\n"))
 
+;;logic here can be used to parse experiences - though the list will start at 1
 (defn parse-list
   "internal fucntion, to turn an item into a list. call to it may look something like:
   (parse-list (get-in resume-parsed [:Personal :contact]) 0 file/path)"
-  [lst len strn]
-  (let [res strn]
-    (if (>= len (count lst))
-      res
-      (parse-list lst (inc len) (str res "\\item " (nth lst len) "\n ")))))
+  ([lst len strn]
+   (let [res strn]
+     (if (>= len (count lst))
+       res
+       (parse-list lst (inc len) (str res "\\item " (nth lst len) "\n ")))))
+  ([lst]
+   (let [res nil
+         len 0]
+     (parse-list lst (inc len) (str res "\\item " (nth lst len) "\n ")))))
+
+(def line-sep "\\noindent\\rule{\\textwidth}{0.4pt}\n")
+
+(defn parse-summary
+  [resume-parsed]
+  (str
+   (get-in resume-parsed
+           [:Summary_Section :summary])))
+
+(defn parse-education
+  [resume-parsed section]
+  (str
+   (get-in resume-parsed [:Education_Section section])))
+
